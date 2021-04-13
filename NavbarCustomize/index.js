4657f808bf5c51b59ed9f0f37159b280b900669b
Tân Dv <mchemistry95@gmail.com>
2021-04-13 11:12:13 +0700

    Update menu extends

--- NavbarCustomize/index.js
+++ NavbarCustomize/index.js
@@ -1,5 +1,5 @@
 $(document).ready(()=> {
-    var isMenuExpaned = true;
+    var isMenuExpaned = false;
     $('#tabs-nav li:first-child').addClass('active');
     $('.tab-content').hide();
     $('.tab-content:first').show();
@@ -7,24 +7,23 @@ $(document).ready(()=> {
 
     $.fn.expandedMenu = function() {
         $('.menu-wrapper li:nth-last-child(1) a i').addClass('show-menu-expanded');
-        $('.menu-customize__tabs').css({'max-height':'200px', 'overflow': 'auto'});
+        $('.menu-customize__tabs').css({'max-height':'200px'});
     }
 
     $.fn.hideMenu = function() {
-        $('.menu-customize__tabs').css({'max-height':'0', 'overflow': 'hidden'});
+        $('.menu-customize__tabs').css({'max-height':'0'});
         $('.menu-wrapper li:nth-last-child(1) a i').removeClass('show-menu-expanded');
     }
 
-    $('.menu-wrapper li:nth-last-child(1)').click(() => {
+    $('.toggle-list-city').click(() => {
         isMenuExpaned = !isMenuExpaned
         if(isMenuExpaned) {
             $().expandedMenu();
         } else {
             $().hideMenu();
-        }
-           
+        } 
     })
-    // Click function
+
     $('#tabs-nav li').click(function(){
         $('#tabs-nav li').removeClass('active');
         $(this).addClass('active');
